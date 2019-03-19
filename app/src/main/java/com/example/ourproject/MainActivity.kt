758f2627919d99cf80bdc96af71c6d8a1dbbdc36
кирил вилиам <kurulaishak2017@gmail.com>
2019-03-19 15:23:11 +0300

    our project

--- app/src/main/java/com/example/ourproject/MainActivity.kt
+++ app/src/main/java/com/example/ourproject/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.ourproject
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val intent1 = Intent(this , System::class.java)
+        val intent2 = Intent(this , Statistics::class.java)
+        setting.setOnClickListener  { startActivity(intent1) }
+        statistics.setOnClickListener { startActivity(intent2) }
     }
+
 }
---
+++ app/src/main/java/com/example/ourproject/System.kt
@@ -0,0 +1,16 @@
+package com.example.ourproject
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class System  : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.systems)
+
+    }
+
+}
